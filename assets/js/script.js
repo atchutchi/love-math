5bcb52845754138607ec638bb5f30adec536dc92
atchutchi <>
2023-05-09 06:29:56 +0000

    Add division game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,6 +48,9 @@ function runGame(gameType) {
     } else if (gameType === "subtract" ) {
         displaySubtractQuestion(num1, num2);
 
+    } else if (gameType === "division" ) {
+        displayDivisionQuestion(num1, num2);
+
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -95,6 +98,9 @@ function calculateCorrectAnswer() {
 
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+
+    } else if (operator === "/") {
+        return [Math.floor(operand1 / operand2), "division"];
 
     } else {
         alert(`Unimplemented operator ${operator}`);
@@ -146,4 +152,12 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
     
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "/";
+
 }
