dd12dd4d68ea9aa30f204d43de747cb3d02e017a
atchutchi <>
2023-05-09 04:55:34 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,20 +10,29 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+    runGame("addition");
 });
 
 /**
 * The main game "loop", called  when  the script  is first  loaded
 * and after the  user's answer as been processed
 */
-function runGame() {
+function runGame(gameType) {
     //Create two random number between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else{
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type : ${gameType}. Aborting!`;
+    }
 
 }
 
@@ -43,7 +52,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
