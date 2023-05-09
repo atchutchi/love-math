296b1e8f7d18abf73fd78a782d569eb30ea38249
atchutchi <>
2023-05-09 06:09:37 +0000

    Add fav icon, and finaly tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,15 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
+
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    });
+
     runGame("addition");
+
 });
 
 /**
@@ -22,6 +30,10 @@ document.addEventListener("DOMContentLoaded", function() {
 * and after the  user's answer as been processed
 */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     //Create two random number between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
